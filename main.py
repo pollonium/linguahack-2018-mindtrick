b42753a01f735b1e0de9c2b1d3fe9436d94af63f
pollonium <pollonium9000@gmail.com>
2018-05-13 00:43:16 +0700

    пропарсены все файлы

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
 import json
+import codecs
 import os
 from checker import Checker
 
-filepath = 'data-raw/'
+# filepath = 'data-raw/'
 original_filepath = 'original/'
-nonoriginal_filepath = 'nonoriginal/'
+# nonoriginal_filepath = 'nonoriginal/'
+#
+# checker = Checker()
+#
+# filenames = os.listdir(filepath)
+# for filename in filenames:
+#     print(filename)
+#     file = (codecs.open(filepath + filename, 'r', encoding='utf-8-sig'))
+#     comment_raw = file.read()
+#     file.close()
+#     comment_pure = json.loads(comment_raw, strict=False)
+#     comment_pure['original'] = checker.is_original(comment_pure['text'])
+#     if comment_pure['original'] == 'yes':
+#         file = open(original_filepath + filename, 'w+')
+#         json.dump(comment_pure, file)
+#     else:
+#         file = open(nonoriginal_filepath + filename, 'w+', encoding='utf-8')
+#         json.dump(comment_pure, file)
 
-checker = Checker()
-
-filenames = os.listdir(filepath)
-for filename in filenames:
-    print(filename)
-    file = open(filepath + filename, 'r', encoding='utf-8')
-    comment_raw = file.read()
-    file.close()
-    comment_pure = json.loads(comment_raw, strict=False)
-    comment_pure['original'] = checker.is_original(comment_pure['text'])
-    if comment_pure['original'] == 'yes':
-        file = open(original_filepath + filename, 'w+')
-        json.dump(comment_pure, file)
-    else:
-        file = open(nonoriginal_filepath + filename, 'w+', encoding='utf-8')
-        json.dump(comment_pure, file)
---
+++ tomita/mygram.cxx
@@ -0,0 +1,4 @@
+#encoding "utf8";
+#GRAMMAR_ROOT S
+
+// S -> Adj Noun; 
