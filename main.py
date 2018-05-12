bc31cbf8032b7bf14f755bf0b813f523e5ac1862
pollonium <pollonium9000@gmail.com>
2018-05-12 19:28:52 +0700

    проверка на антиплагиате

---
+++ main.py
@@ -0,0 +1,21 @@
+import json
+import os
+from checker import Checker
+
+filepath = 'data-raw/'
+
+filenames = os.listdir(filepath)
+filenames.remove('.gitkeep')
+# print(filenames)
+# filenames = ['14_8.txt']
+
+checker = Checker()
+
+for filename in filenames:
+    print(filename)
+    file = open(filepath + filename, 'r', encoding='utf-8')
+    comment_raw = file.read()
+    comment_pure = json.loads(comment_raw, strict=False)
+    comment_pure['original'] = checker.is_original(comment_pure['text'])
+
+    print(comment_pure['original'])
