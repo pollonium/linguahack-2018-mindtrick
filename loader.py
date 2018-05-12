86a40c45f404cbab6abfb77d89540ec3985af0ee
Kuligin Sergey <kylserpie@gmail.com>
2018-05-12 16:49:41 +0700

    открытие файла и импорт словаря

--- loader.py
+++ loader.py
@@ -1,11 +1,12 @@
 import json
-stt = 'js.txt'
+
 def load_data(filepath):
-        with open(filepatch,'r') as myfile:
-            return json.load(myfile)
-        
-def pretty_print_json(data):
-        for st in data:
-            print(json.dumps(st, indent="   ",ensure_ascii=False, sort_keys=True))
-            
-pretty_print_json(load_data(stt))        
+        file = open(filepath, 'r')
+        data = file.read()
+
+        return json.loads(data)
+
+
+
+
+
