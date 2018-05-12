3e30aa75f0a700c806f22c855ceb96453ed49de6
pollonium <pollonium9000@gmail.com>
2018-05-12 20:28:17 +0700

    пропарсены первые ~50 файлов

--- main.py
+++ main.py
@@ -3,18 +3,22 @@ import os
 from checker import Checker
 
 filepath = 'data-raw/'
-
-filenames = os.listdir(filepath)
-# print(filenames)
-# filenames = ['14_8.txt']
+original_filepath = 'original/'
+nonoriginal_filepath = 'nonoriginal/'
 
 checker = Checker()
 
+filenames = os.listdir(filepath)
 for filename in filenames:
     print(filename)
     file = open(filepath + filename, 'r', encoding='utf-8')
     comment_raw = file.read()
+    file.close()
     comment_pure = json.loads(comment_raw, strict=False)
     comment_pure['original'] = checker.is_original(comment_pure['text'])
-
-    print(comment_pure['original'])
+    if comment_pure['original'] == 'yes':
+        file = open(original_filepath + filename, 'w+')
+        json.dump(comment_pure, file)
+    else:
+        file = open(nonoriginal_filepath + filename, 'w+', encoding='utf-8')
+        json.dump(comment_pure, file)
