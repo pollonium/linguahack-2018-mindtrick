e226c019bbe6d951d9508dfc327fb5ced2a1a54b
pollonium <pollonium9000@gmail.com>
2018-05-13 15:31:42 +0700

    [REFACTOR] выделила код из main, чтобы облегчить предоставление параметров

---
+++ analyzer.py
@@ -0,0 +1,66 @@
+import os
+import bs4
+from plagiarism_checker import Checker
+from tomita_executor import Executor
+from mp_stemmer import Stemmer
+
+
+class Analyzer:
+    secret_key = ''
+    payload = {
+        'rawDataDir': 'data-raw/',
+        'originalTextsDir': 'original/',
+        'nonoriginalTextsDir': 'nonoriginal/',
+        'TomitaOutput': 'facts.txt',
+        'MPStemmerOutput': 'stems.txt',
+    }
+    words = {}
+
+    def __init__(self, secret_key, payload=None):
+        self.secret_key = secret_key
+        if payload:
+            for key, entry in self.payload.items():
+                if key in payload:
+                    self.payload[key] = payload[key]
+
+    # Отделяем в комментариях оригиналы от шаблонов
+    def categorize_texts(self):
+        checker = Checker(self.secret_key)
+        filenames_to_check = os.listdir(self.payload['rawDataDir'])
+        for filename in filenames_to_check:
+            with open(self.payload['rawDataDir'] + filename, 'r', encoding='utf-8') as file:
+                comment = file.read()
+            # процентная доля оригинальности должна быть больше или равна 70
+            # При установке is_test=1 с сервера приходят случайно сгенерированный показатель оригинальности текста
+            text_is_original = checker.is_original(comment, percentage=70, is_test=1)
+            directory = self.payload['originalTextsDir'] if text_is_original else self.payload['nonoriginalTextsDir']
+            file = open(directory + filename, 'w+', encoding='utf-8')
+            file.write(comment)
+            file.close()
+
+    # Выделяем список наиболее частотных слов в отзывах
+    def calculate_word_references(self, prev_filename):
+        parser = Executor(prev_filename)
+        filenames = os.listdir(self.payload['originalTextsDir'])
+        for filename in filenames:
+            parser.execute(filename)
+            with open(self.payload['TomitaOutput'], 'r', encoding='utf-8-sig') as file:
+                data = file.read()
+                soup = bs4.BeautifulSoup(data, 'lxml')
+            # В результирующем файле слова хранятся в элементе следующего формата:
+            # <SignificantWord><Value val="СОТРУДНИК"/></SignificantWord>
+            # Так как тег <Value> не встречается больше ни в каких элементах, то искать нужные слова можно сразу по нему
+            # + sp4 преобразует имена элементов в lowercase
+            tags = soup("value")
+            for tag in tags:
+                word = tag.get('val')
+                if word in self.words:
+                    self.words[word] += 1
+                else:
+                    self.words[word] = 1
+
+    def write_stem_references(self):
+        # Используем стеммер Портера для русского языка для группировки слов по корню
+        stemmer = Stemmer()
+        stemmer.process(self.words)
+        stemmer.print_to_file(self.payload['MPStemmerOutput'])
--- main.py
+++ main.py
@@ -1,67 +1,11 @@
-import json
-import codecs
-import os
-from checker import Checker
-from tomita_executor import Executor
-from algporter2 import Porter
-import bs4  # https://www.crummy.com/software/BeautifulSoup/bs4/doc/
+from analyzer import Analyzer
 
 
-# filepath = 'data-raw/'
-original_filepath = 'original/'
-# nonoriginal_filepath = 'nonoriginal/'
+# данные для работы кода
+secret_key = 'g96Xpx8RUpIq893'
+prevFilename = '9_9.txt'
 
-# checker = Checker()
-# for filename in filenames:
-#     print(filename)
-#     file = (codecs.open(filepath + filename, 'r', encoding='utf-8-sig'))
-#     comment_raw = file.read()
-#     file.close()
-#     comment_pure = json.loads(comment_raw, strict=False)
-
-#     comment_pure['original'] = checker.is_original(comment_pure['text'])
-#     if comment_pure['original'] == 'yes':
-#         file = open(original_filepath + filename, 'w+')
-#         json.dump(comment_pure, file)
-#     else:
-#         file = open(nonoriginal_filepath + filename, 'w+', encoding='utf-8')
-#         json.dump(comment_pure, file)
-#
-
-# filenames = os.listdir(original_filepath)
-# filenames = ['1_2.txt']
-# for filename in filenames:
-    # parser.execute(filename)
-   # file = (codecs.open(original_filepath + filename, 'r', encoding='utf-8-sig'))
-   #  file = (codecs.open(original_filepath + filename, 'r', encoding='utf-8-sig'))
-   #  comment_raw = file.read()
-   #  file.close()
-   #  comment_pure = json.loads(comment_raw, strict=False)
-   #  file = open(filename, 'w+', encoding='utf-8')
-   #  file.write(comment_pure['text'])
-#     file.close()
-
-# <SignificantWord><Value val="СОТРУДНИК"/></SignificantWord> -- sp4 хранит в итоге имена атрибутов в lowercase
-values = {}
-page = 'facts.txt'
-parser = Executor()
-filenames = os.listdir(original_filepath)
-for filename in filenames:
-    parser.execute(filename)
-    file = open(page, 'r', encoding='utf-8-sig')
-    data = file.read()
-    soup = bs4.BeautifulSoup(data, 'lxml')
-    file.close()
-
-    raw_values = soup("value")
-    for raw_value in raw_values:
-        value = raw_value.get('val')
-        if value in values:
-            values[value] += 1
-        else:
-            values[value] = 1
-
-stemmer = Porter()
-stemmer.process(values)
-
-print('yaay!')
+analyzer = Analyzer(secret_key)
+analyzer.categorize_texts()
+analyzer.calculate_word_references(prevFilename)
+analyzer.write_stem_references()
